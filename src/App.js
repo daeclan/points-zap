4d56bc80e8ac158ddecda7dc70f16544f2a1fdee
Declan Murphy <declxn@gmail.com>
2021-09-30 20:39:33 -0500

    There is light now, and ambient light hits right.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { useFrame } from 'react-three-fiber'
+
+function Cube(props) {
+  return (
+    <mesh {...props}>
+      <boxGeometry args={[5, 5, 5]} />
+      <meshStandardMaterial color="salmon" />
+    </mesh>
+  )
+}
 
 export default function App() {
   return (
     <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color="salmon" />
-      </mesh>
+      <Cube size={[5]} />
+      <ambientLight />
+      <Cube position={[100.5, 1, 1]} />
+      <Cube position={[-100.5, 1, 1]} />
     </Canvas>
   )
 }
