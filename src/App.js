374ce98c8dadd81a4ea38bf21fda9df52373dce7
Declan Murphy <declxn@gmail.com>
2021-09-30 20:25:17 -0500

    A fkn @ symbol was the key here, spent so much time over stuffing this with reinstallations of npm this and that. All it took was @react-three/fiber and not react-three/fiber. JFC.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React, { useState } from "react"
-import ReactDOM from "react-dom"
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
 
-function Foo() {
-  const [count, setCount] = useState(0)
-  return <div style={{ opacity: 1 }} />
-}
-
-ReactDOM.render(<Foo />, document.getElementById("root"))
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas>
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial color="salmon" />
+      </mesh>
+    </Canvas>
+  )
 }
-
-export default App;
